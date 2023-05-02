979547230b5ed8a958c208b1d999a3d36a097ce5
Janic Bijlhout <s2658887@vuw.leidenuniv.nl>
2023-05-02 16:36:57 +0200

    two plots

--- script.R
+++ script.R
@@ -1,26 +1,24 @@
-# Removing outliers with adjusted criteria
-
-#Type 1 error: Rejecting the null-hypothesis while it is true.
-#Type 2 error: Failing to rejecting a null-hypothesis that is not true.
-# QRPs are used to get the p-value below the alpha level aka p-hacking
-# an outlier is 3 stddev above or below the mean
-# selecting a particular criterion for the most desirable solution
-
 # Simulate data with an outlier
 set.seed(123)
-data <- c(rnorm(9), 50)
+data <- c(rnorm(17, mean = 5, sd = 2), rnorm(3, mean = 20, sd = 5))
 
 # Identify the outlier using the median absolute deviation (MAD) method
 mad <- median(abs(data - median(data)))
-outlier <- which(abs(data - median(data)) > 3 * mad)
+outliers <- which(abs(data - median(data)) > 2 * mad)
 
-# Replace the outlier with the median of the data
-data[outlier] <- median(data)
+# Set up a multi-panel layout for two plots side by side
+par(mfrow=c(1,2))
 
-# Plot the data
+# Plot the data with outliers
 plot(data, pch = 16, cex = 1.5, col = "blue", xlab = "Observation", ylab = "Value")
 if (length(outlier) > 0) {
   points(outlier, data[outlier], pch = 16, cex = 1.5, col = "red")
-  legend("topright", legend = "Outlier", pch = 16, col = "red", cex = 1.2)
+  legend("topleft", legend = "Outlier", pch = 16, col = "red", cex = 1.2)
 }
 
+# Replace the outliers with the median of the data
+data_without_outliers <- data
+data_without_outliers[outliers] <- median(data)
+
+# Plot the data without outliers
+plot(data_without_outliers, pch = 16, cex = 1.5, col = "blue", xlab = "Observation", ylab = "Value")
